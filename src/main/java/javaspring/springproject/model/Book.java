066047e69a41efa037bbd3ccfa13c145cc73931e
MaJl4uLLlka <70745048+MaJl4uLLlka@users.noreply.github.com>
2021-09-22 14:56:29 +0300

    1.1

---
+++ src/main/java/javaspring/springproject/model/Book.java
@@ -0,0 +1,12 @@
+package javaspring.springproject.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+}
