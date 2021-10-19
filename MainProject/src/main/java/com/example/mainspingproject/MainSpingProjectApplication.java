8b9749ca38a376eb79c6099bfdb306fb0896e888
MaJl4uLLlka <70745048+MaJl4uLLlka@users.noreply.github.com>
2021-10-19 11:39:20 +0300

    Main Project folder

---
+++ MainProject/src/main/java/com/example/mainspingproject/MainSpingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainspingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainSpingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainSpingProjectApplication.class, args);
+    }
+
+}
